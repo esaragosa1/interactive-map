cf2c29499e22c5d3697640e813afbf70b09697b2
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-10-15 20:18:41 -0700

    modified fetch and others

--- mapapp.js
+++ mapapp.js
@@ -57,7 +57,7 @@ async function foursquare (business){
 	let limit = 5
 	let lat = myMap.coordinates[0]
 	let lon = myMap.coordinates[1] 
-    let response= await fetch(`https://api.foursquare.com/v3/places/search?query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+    let response= await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
     let data = await response.text();
     let parsedData = JSON.parse(data);
     let bizresults = parsedData.results
